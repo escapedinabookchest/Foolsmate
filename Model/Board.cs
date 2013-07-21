31636b0fc1539da6199bc52ec400cda460046fe1
nope <escapedinabookchest@gmail.com>
2013-07-21 23:08:42 +0200

    Board and Pieces

---
+++ Model/Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foolsmate.Model
+{
+    /*
+     * This class is responsible for keeping track of the pieces on the board.
+     */
+    internal class Board
+    {
+        public const int number_of_ranks = 8;
+        public const int number_of_files = 8;
+
+        private Piece[,] grid;
+
+        public Board()
+        {
+            grid = new Piece[number_of_ranks, number_of_files];
+        }
+
+        public Piece this[int x, int y]
+        {
+            get { return Grid[x, y]; }
+        }
+
+        public Piece[,] Grid
+        {
+            get { return this.grid; }
+            private set { this.grid = value; }
+        }
+
+        public void AddPiece(int x, int y, Piece piece)
+        {
+            Grid[x, y] = piece;
+        }
+
+        public void RemovePiece(int x, int y)
+        {
+            Grid[x, y] = new Piece();
+        }
+
+        //REFACTOR: Move to another class responsible for moving a piece?
+        public void MovePiece(int x1, int y1, int x2, int y2)
+        {
+            AddPiece(x2, y2, new Piece(this[x1, y1].Rank, this[x1, y1].Color));
+            RemovePiece(x1, y1);
+        }
+    }
+}
---
+++ Model/Piece.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foolsmate.Model
+{
+    internal struct Piece
+    {
+        private readonly Rank rank;
+        private readonly Color color;
+        private readonly bool isSet;
+
+        public Piece(Rank rank, Color color)
+        {
+            this.rank = rank;
+            this.color = color;
+            this.isSet = true;
+        }
+
+        public Rank Rank
+        {
+            get { return this.rank; }
+        }
+
+        public Color Color
+        {
+            get { return this.color; }
+        }
+
+        public bool IsSet
+        {
+            get { return this.isSet; }
+        }
+    }
+}
